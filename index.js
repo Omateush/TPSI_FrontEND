8f1abdb4b8007e852b0644b5f5b417207fd705a6
Mateus <145764702+Omateush@users.noreply.github.com>
2024-03-05 19:56:34 +0000

    lista de tarefas

--- index.js
+++ index.js
@@ -3,12 +3,9 @@ const app = Vue.createApp(
         data() 
         {
             return {
-            productName: "Navio",
-            Productsubtitle: "Nao se afogue.",
-            ProductDescription: "Cruise ship",
-            ship: "https://i0.statig.com.br/bancodeimagens/45/bv/jk/45bvjkhy48luu055s2z69ieoa.jpg",
-            subtitleStyle:{'background-color' : 'brown', 'color' : 'blue'}
-            // additional properties later
+                userInput: 0,
+                cpuRandom: Math.float(Math.random()*11),
+                userTries: [],
         }
     }
 })
